ce1d958f463b2836e34b3275dd67c3b8936e4d91
Timoha <66478612+TimohaDev@users.noreply.github.com>
2021-01-14 19:55:00 +0200

    Update json.py

--- commands/json.py
+++ commands/json.py
@@ -3,4 +3,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command('json', ['!']))
 def json(client, message):
-    message.reply_text(message)
+    message.reply_text(message.reply_to_message)
